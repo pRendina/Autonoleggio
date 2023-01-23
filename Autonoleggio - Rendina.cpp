56784593d24377f8508e0dbce5a2ca707ab3c355
pRendina <122788489+pRendina@users.noreply.github.com>
2023-01-23 16:54:06 +0100

    Add files via upload

--- Autonoleggio - Rendina.cpp
+++ Autonoleggio - Rendina.cpp
@@ -11,24 +11,40 @@ struct auton{
     string marca;
     string modello;
     string colore;
-    string g[7];
+    string lun,mar,mer,gio,ven,sab,dom;
     bool free;
 };
+
+int sizee()
+{
+    ifstream fin(FILE_NAME, ios::app);
+    string app;
+    int i = 0;
+    while(getline(fin, app))
+        i++;
+    fin.close();
+    return i;
+}
 
 void riempi_vet(auton autovett, auton vet[])
 {
     ifstream fin(FILE_NAME,ios::in);
-
-    string app;
+    int i = 0;
     while(!fin.eof())
     {
-        getline(fin,app);
-        for(int i=0; i<7; i++)
-        {
-            fin>>vet[i].categoria>>vet[i].marca>>vet[i].modello>>vet[i].colore;
-            for(int k=0; k<7; k++)
-                fin>>vet[i].g[k];
-        }
+        getline(fin,vet[i].categoria, ',');
+        getline(fin,vet[i].marca, ',');
+        getline(fin,vet[i].modello, ',');
+        getline(fin,vet[i].colore, ',');
+
+        getline(fin,vet[i].lun, ',');
+        getline(fin,vet[i].mar, ',');
+        getline(fin,vet[i].mer, ',');
+        getline(fin,vet[i].gio, ',');
+        getline(fin,vet[i].ven, ',');
+        getline(fin,vet[i].sab, ',');
+        getline(fin,vet[i].dom);
+        i++;
     }
     fin.close();
 }
@@ -54,21 +70,91 @@ void stampa_catalogo(auton autovett, auton *ptr_auto, auton vet[])
     fin.close();
 }
 
-void findA(auton vet[], string sett, string g, int cont)
+void cambia_file(auton vet[])
 {
-    for(int i=0; i<7; i++){
-        if(g == sett){
-            if(vet[i].g[cont] == "L"){
-                cout<<endl<<vet[i].marca<<" "<<vet[i].modello<<" "<<vet[i].colore<<" ";
-            }
+
+}
+
+void findA(auton vet[], string cate)
+{
+    string sett_vet[]{"Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"};
+    for(int i=0; i<sizee(); i++){
+        if(vet[i].categoria == cate){
+            cout<<i+1<<") ";
+            cout<<vet[i].marca<<" "<<vet[i].modello<<" "<<vet[i].colore;
+            cout<<"\n\tDisponibilita':\n"<<
+            "\t\tLunedi': "<<vet[i].lun<<
+            "\n\t\tMartedi': "<<vet[i].mar<<
+            "\n\t\tMercoledi': "<<vet[i].mer<<
+            "\n\t\tGiovedi': "<<vet[i].gio<<
+            "\n\t\tVenerdi': "<<vet[i].ven<<
+            "\n\t\tSabato: "<<vet[i].sab<<
+            "\n\t\tDomenica: "<<vet[i].dom<<endl<<endl;
         }
     }
+
+    int select,cont = 0;;
+    cout<<"Inserire macchina da selezionare: ";
+    cin>>select;
+
+    string g[7];
+    do{
+        cout<<"\nInserire giorni da prenotare (inserire ESCI per uscire): ";
+        cin>>g[cont];
+        cont++;
+    }while(g[cont-1] != "ESCI");
+
+    for(int i=0; i<7; i++){
+        if(g[i]=="") break;
+
+        if(g[i] == sett_vet[i]){
+            if(vet[select-1].lun == "A")
+                cout<<"Macchina occupata quel giorno\n";
+            else cambia_file(vet);
+        }
+
+        if(g[i] == sett_vet[i]){
+            if(vet[select-1].mar == "A")
+                cout<<"Macchina occupata quel giorno\n";
+            else cambia_file(vet);
+        }
+
+        if(g[i] == sett_vet[i]){
+            if(vet[select-1].mer == "A")
+                cout<<"Macchina occupata quel giorno\n";
+            else cambia_file(vet);
+        }
+
+        if(g[i] == sett_vet[i]){
+            if(vet[select-1].gio == "A")
+                cout<<"Macchina occupata quel giorno\n";
+            else cambia_file(vet);
+        }
+
+        if(g[i] == sett_vet[i]){
+            if(vet[select-1].ven == "A")
+                cout<<"Macchina occupata quel giorno\n";
+            else cambia_file(vet);
+        }
+
+        if(g[i] == sett_vet[i]){
+            if(vet[select-1].sab == "A")
+                cout<<"Macchina occupata quel giorno\n";
+            else cambia_file(vet);
+        }
+
+        if(g[i] == sett_vet[i]){
+            if(vet[select-1].dom == "A")
+                cout<<"Macchina occupata quel giorno\n";
+            else cambia_file(vet);
+        }
+
+    }
+
 }
 
 void cerca_vettura(auton autovett, auton vet[])
 {
-
-  //  ifstream fin(FILE_NAME, ios::in);
     string cate;
 
     cout<<"Inserire categoria dell'auto: ";
@@ -76,28 +162,19 @@ void cerca_vettura(auton autovett, auton vet[])
     if(cate!="utilitaria" && cate!="lusso" && cate!="sportiva" && cate!="furgone")
         cout<<"Categoria non esistente\n";
     else{
-        string g, sett_vet[]{"Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"};
-        cout<<"Inserire giorno da prenotare: ";
-        cin>>g;
-
         if(cate == "utilitaria"){
-            for(int k=0; k<7; k++)
-                findA(vet, sett_vet[k], g, k);
+            findA(vet, cate);
         }
         if(cate == "lusso"){
-            for(int k=0; k<7; k++)
-                findA(vet, sett_vet[k], g, k);
+            findA(vet, cate);
         }
         if(cate == "sportiva"){
-            for(int k=0; k<7; k++)
-                findA(vet, sett_vet[k], g, k);
+            findA(vet, cate);
         }
         if(cate == "furgone"){
-            for(int k=0; k<7; k++)
-                findA(vet, sett_vet[k], g, k);
+            findA(vet, cate);
         }
     }
-
 }
 
 void menu()
@@ -106,15 +183,22 @@ void menu()
     int scelta;
 
     riempi_vet(autovett, vet);
-    cout<<"\n\t\tMENU'"<<endl;
-    cout<<"1 - Consulta catalogo"<<endl
-        <<"2 - Ricerca vettura"<<endl;
+    while(scelta!=3){
+        cout<<"\n\t\tMENU'"<<endl;
+        cout<<"1 - Consulta catalogo"<<endl
+            <<"2 - Ricerca vettura"<<endl
+            <<"3 - Uscita"<<endl;
 
-    cin>>scelta;
-    switch(scelta){
-        case 1: stampa_catalogo(autovett, ptr_auto, vet);
-                break;
-        case 2: cerca_vettura(autovett, vet);
+        cin>>scelta;
+        switch(scelta){
+            case 1: stampa_catalogo(autovett, ptr_auto, vet);
+                    break;
+            case 2: cerca_vettura(autovett, vet);
+                    break;
+            case 3: cout<<"USCITA...";
+                    break;
+            default: cout<<"Scelta sbagliata\n";
+        }
     }
 }
 
